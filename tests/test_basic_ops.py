5cfd3f398b0f4ddcbc310dfb1733b90f5e6c80ed
krande <kristoffer_andersen@outlook.com>
2023-12-10 17:31:35 +0100

    add basic testing

---
+++ tests/test_basic_ops.py
@@ -0,0 +1,11 @@
+import pathlib
+import os
+
+
+def test_basic_toml_check(root_dir):
+    action_toml = root_dir / "action_config.toml"
+    os.environ["GITHUB_OUTPUT"] = "temp/output.txt"
+    os.environ["CONFIG_TOML_FILE"] = action_toml.as_posix()
+    os.environ["PYPROJECT_TOML_FILE"] = (root_dir / "pyproject.toml").as_posix()
+    os.makedirs(os.path.dirname(os.environ["GITHUB_OUTPUT"]), exist_ok=True)
+    check_toml()
